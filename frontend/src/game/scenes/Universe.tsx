b144b28eae4179e45b54904e05d9e5fd1db8487e
kamzhanyue <kamzhanyue@gmail.com>
2024-05-23 17:28:33 +0900

    Implement Stars within a grid 

--- frontend/src/game/scenes/Universe.tsx
+++ frontend/src/game/scenes/Universe.tsx
@@ -3,6 +3,7 @@ import SolarSystem from "../classes/SolarSystem.ts";
 
 export class Universe extends Scene {
     private solarSystem?: SolarSystem;
+
     constructor() {
         super({ key: 'Universe' });
     }
@@ -12,11 +13,11 @@ export class Universe extends Scene {
     }
 
     create() {
-        this.cameras.main.zoom = 10;
+        this.cameras.main.zoom = 2;
         const centerX = this.cameras.main.centerX;
         const centerY = this.cameras.main.centerY;
 
-        this.add.shader('starShader', centerX, centerY, this.scale.width, this.scale.height);
+        this.add.shader('starShader', centerX, centerY, 1000, 1000);
         this.anims.create({
             key: 'earth_spin',
             frames: this.anims.generateFrameNumbers('earth', {frames:[0,1,2,3,4,5]}),
--- frontend/src/game/shaders/starShader.glsl
+++ frontend/src/game/shaders/starShader.glsl
@@ -1,27 +1,51 @@
-#ifdef GL_ES
+
 precision mediump float;
-#endif
 
-uniform vec2 resolution;
 uniform float time;
+uniform vec2 resolution;
 
-float random(vec2 st) {
-    return fract(sin(dot(st.xy, vec2(12.9898, 78.233))) * 43758.5453123);
+// 2D rotation matrix
+mat2 rot(float a) {
+    float s = sin(a), c = cos(a);
+    return mat2(c, -s, s, c);
+}
+
+float star(vec2 uv, float flare) {
+    float d = length(uv);
+    float m = .015/d;
+
+    // create horizontal and vertical rays
+    float rays = max(0., 1.-abs(uv.x * uv.y * 5000.));
+    m += rays * flare;
+
+    // rotate the rays by 45 degrees
+    uv*=rot(3.1415*0.25);
+    rays = max(0., 1.-abs(uv.x * uv.y * 5000.));
+    m += rays*.3 * flare;
+
+    return m;
+}
+
+// pseudo random generator to take two numbers into one
+float hash21(vec2 p) {
+    p = fract(p*vec2(123.34, 456.21));
+    p += dot(p, p+45.32);
+    return fract(p.x*p.y);
 }
 
 void main() {
-    vec2 st = gl_FragCoord.xy / resolution.xy;
-    st.x *= resolution.x / resolution.y; // maintain aspect ratio
+    vec2 uv = (gl_FragCoord.xy -.5*resolution.xy)/resolution.y;
+    uv *= 5.;
+    vec3 col = vec3(0);
 
-    vec3 color = vec3(0.0);
-    float brightness = random(st);
+    // create a grid uv and an id for each box
+    vec2 gv = fract(uv)-.5;
+    vec2 id = floor(uv);
+    float n = hash21(id); // random between 0 and 1
 
-    // Adjust this threshold to control star density
-    if (brightness > 0.95) {
-        // Draw larger points for stars
-        gl_PointSize = 2.0; // Adjust this value for star size
-        color = vec3(1.0); // star color (white)
-    }
+    col += star(gv-vec2(n, fract(n*34.))+.5, 1.);
 
-    gl_FragColor = vec4(color, 1.0);
+    // debugging the grid
+    if(gv.x > .48 || gv.y > .48) col.r = 1.;
+    gl_FragColor = vec4(col, 1.0);
 }
