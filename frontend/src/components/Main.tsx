6720199be7fa1816f6b7366f8aae5a109332a63c
kamzhanyue <kamzhanyue@gmail.com>
2024-06-02 02:40:46 +1000

    Fix typescript build errors

--- frontend/src/components/Main.tsx
+++ frontend/src/components/Main.tsx
@@ -1,23 +1,9 @@
 import React, {useEffect, useRef, useContext} from 'react';
-import styled from "styled-components";
 import StoryComponent from "./StoryComponent.tsx";
 import Game from "../game/Game.tsx";
 import {Universe} from "../game/scenes/Universe.tsx";
 import MirrorComponent from "./MirrorComponent.tsx";
 import {GameContext, GameContextType, GameState} from "../contexts/GameContext.tsx";
-
-const Overlay = styled.div`
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  display: flex;
-  flex-direction: column; /* Display choices vertically */
-  align-items: center; /* Center horizontally */
-`
-
-const StartButton = styled.button`
-`
 
 const Main: React.FC = () => {
     const universeRef = useRef<Universe>(null);
