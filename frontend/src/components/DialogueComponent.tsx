7fc7b173c2355e3d8f516e61b0014702d4f37c0f
kamzhanyue <kamzhanyue@gmail.com>
2024-06-21 05:52:30 +0800

    Start on Character Popup

--- frontend/src/components/DialogueComponent.tsx
+++ frontend/src/components/DialogueComponent.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import Typewriter from "./Typewriter.tsx";
 import DialogueBox from "./DialogueBox.tsx";
+import {motion} from "framer-motion";
+import {characters} from "../setup/Character.ts";
 
 const TitleText = styled.div`
   font-size: 40px;
@@ -18,6 +20,28 @@ const Separator = styled.div`
   margin: 8px 0; /* Adjust margin */
 `
 
+const CharacterContainer = styled(motion.div)`
+  position: fixed;
+  bottom: 200px;
+  top: 120px;
+  left: 20px;
+  right: 20px;
+  border: 5px white solid;
+  border-radius: 5px;
+`
+
+const Character = styled(motion.img)`
+  image-rendering: pixelated;
+  image-rendering: -moz-crisp-edges;
+  image-rendering: crisp-edges;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover; // Ensures the image covers the area while maintaining aspect ratio
+  z-index: -1;      // Make sure it is behind the overlay
+`
+
 interface DialogueComponentProps {
     text: string;
     next?: () => void;
@@ -29,8 +53,28 @@ const DialogueComponent: React.FC<DialogueComponentProps> = ({ text, next }) => 
     const characterName = colonIndex !== -1 ? text.substring(0, colonIndex).trim() : '';
     const dialogueBody = colonIndex !== -1 ? text.substring(colonIndex + 1).trim() : text;
 
+    const getCharacter = (): string => {
+        if(characterName === "Ship") {
+            return `../assets/characters/${characters[characterName]}`;
+        } else {
+            return `../assets/characters/${characters[characterName]}`;
+        }
+    }
+
     return (
         <>
+            {characterName && characterName in characters &&
+                <>
+                    <CharacterContainer
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0, transition: {duration: 1} }}
+                        transition={{ duration: 1 }}>
+                        <Character src={getCharacter()} alt='character'/>
+                    </CharacterContainer>
+                </>
+            }
+
             <DialogueBox>
                 {/* Render character name, separator, and dialogue body separately if character name exists */}
                 {characterName &&
